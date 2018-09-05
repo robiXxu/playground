b9359a30f9963102613ce02f8bf62035bd6908c1
Schiriac Robert <schiriac.robert@gmail.com>
2018-09-05 22:02:26 +0300

    test

---
+++ getNestedObjectById.js
@@ -0,0 +1,64 @@
+const root = {    
+   "objects":[
+       {
+           "id" : "id_1",
+           "type" : "Unit",
+           "name" : "name_1",
+           "isPlayer" : true,
+           "isPlayable" : true,
+           "designation" : "Grenadier",
+           "health" : 1,
+            "children" : [
+                {
+                    "id" : "id_16",
+                    "type" : "Group",
+                    "name" : "Group 1",
+                    "isPlayable" : true,
+                    "designation" : "Grenadier",
+                   "health" : 1,
+                   "children":[
+                       {
+                           "id" : "id_17",
+                           "type" : "Unit",
+                           "name" : "Antonio Banderas",
+                           "isPlayable" : true,
+                           "designation" : "Sniper",
+                           "health" : 1
+                       }
+                   ]
+                }
+            ]
+       },
+       {
+           "id" : "id_1_1",
+           "type" : "Unit",
+           "name" : "name_1_1",
+           "isPlayable" : true,
+           "designation" : "Grenadier",
+           "health" : 1
+       },
+       {
+           "id" : "id_1_2",
+           "type" : "Unit",
+           "name" : "name_1_2",
+           "isPlayable" : true,
+           "designation" : "Grenadier",
+           "health" : 1
+
+       }
+   ]
+};
+
+const id = 'id_17';
+
+const getNestedObjectById = ( array, id ) =>
+    array.reduce((object, item) => {
+        return item.id === id ? 
+            item : 
+            !!item.children ? 
+                getNestedObjectById( item.children, id ) :
+                object;
+    },{});
+
+const obj = getNestedObjectById(root.objects, id);
+console.log(obj);

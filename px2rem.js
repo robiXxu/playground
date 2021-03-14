2882ca7b25b4cea431ed6b28d48b1d242d322612
Schiriac Robert <schiriac.robert@gmail.com>
2021-03-14 19:27:43 +0200

    px2rem

--- px2rem.js
+++ px2rem.js
@@ -3,7 +3,7 @@ const fontBaseSize = 16;
 const px2Rem = (min, max, step = 1) => {
   const range = [];
   for (let i = min; (max - i) * step >= 0; i += step) {
-    range.push(i.toFixed(2));
+    range.push(i > 0 ? i : i.toFixed(2));
   }
   return range.reduce((obj, v) => {
     obj[`${v}px`] = `${v / fontBaseSize}rem`;
@@ -11,6 +11,6 @@ const px2Rem = (min, max, step = 1) => {
   }, {});
 };
 
-console.log(px2Rem(10, 14, 2));
+// console.log(px2Rem(10, 14, 2));
 
-console.log(px2Rem(-1, 1, 0.05));
+console.log(px2Rem(-2, 2, 0.05));
